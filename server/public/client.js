a0cd4784b9ab44cf669b62335fcfe6c0cb68b9ea
CJ Simon <cjsimon250@outlook.com>
2023-01-28 15:14:19 -0600

    added function to add values to imput feild and started on submit function

--- server/public/client.js
+++ server/public/client.js
@@ -1,5 +1,27 @@
 $(document).ready(onReady);
 
 function onReady(){
+    $('.calculation-btns-box').on('click', 
+    'button:not(#btn-equals)',
+     addValueToInputfield);
 
+    $('#btn-equals').on('click', onSubmitInputs);
+
+
+}
+
+//function to add values to the input field
+function addValueToInputfield(){
+  let id = $(this).attr('id');
+  let buttonValue = $(`#${id}`).text();
+  let currentInputValue = $('#calculation-input').val();
+
+   $('#calculation-input').val(currentInputValue + buttonValue)
+};
+
+//function to run on "="
+function onSubmitInputs(){
+    let input = $('#calculation-input').val();
+
+    console.log(input)
 }
